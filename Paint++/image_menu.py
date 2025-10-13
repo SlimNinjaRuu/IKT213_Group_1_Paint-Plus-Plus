6f7ba6f45a8881f4e3829281e0adaa633cb89d38
Petter Berg Andersen <petterba@uia.no>
2025-10-13 10:24:07 +0200

    Petter sin branch

--- Paint++/image_menu.py
+++ Paint++/image_menu.py
@@ -1,9 +1,82 @@
 import cv2
 import numpy as np
 
-def crop(image, x_0, x_1,  y_0, y_1):
-    cropped_image = image[y_0:y_1, x_0:x_1]
-    return cropped_image
+def click_event(event, x, y, flags, param):
+    disp = param["disp"]
+    points = param["points"]
+
+    # Showing vertices for polygon as dots on the image when m1 is given
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append((x, y))
+        cv2.circle(disp, (x, y), 4, (255, 0, 0), -1, lineType=cv2.LINE_AA)
+        cv2.imshow("Pick polygon", disp)
+
+def polygon_points(image):
+    # Getting the vertices coordinates until enter is given
+    points = []
+    disp = image.copy()
+    cv2.namedWindow("Pick polygon", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("Pick polygon", disp)
+    cv2.setMouseCallback("Pick polygon", click_event, {"disp": disp, "points": points})
+
+    # Key loop: Enter -> done, Esc -> cancel
+    while True:
+        key = cv2.waitKey(20) & 0xFF
+        if key in (13, 10):   # Enter/Return
+            break
+        if key == 27:         # Esc
+            points.clear()
+            break
+
+    cv2.destroyWindow("Pick polygon")
+    return np.array(points, dtype=np.int32)
+
+def polygon_selection(image):
+    pts = polygon_points(image)
+    if pts.shape[0] < 3:
+        # 3 or more points are needed to make a polygon
+        return image.copy()
+
+    # Making a mask
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    cv2.fillPoly(mask, [pts], 255)
+
+    # Manipulation of the whole image
+    image_manipulation = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    shape_bgr = cv2.cvtColor(image_manipulation, cv2.COLOR_GRAY2BGR)
+
+    # Manipulation is copied only to the area inside of polygon
+    out = image.copy()
+    out[mask > 0] = shape_bgr[mask > 0]
+
+    return out
+
+def rectangular_selection(image):
+    # x and y coordinate for top left selected corner
+    # width and height for marked rectangle
+    x, y, w, h = cv2.selectROI("Rectangular select", image)
+    cv2.destroyWindow("Rectangular select")
+
+    out = image.copy()
+    roi = image[y:y+h, x:x+w]
+
+    # Making selected area gray
+    roi_manipulation = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    # Making the gray selected area back to 3 channels for compatibility
+    #  This has to be changed in order to do other operations to the selected area
+    roi_compatible = cv2.cvtColor(roi_manipulation, cv2.COLOR_GRAY2BGR)
+
+    out[y:y+h, x:x+w] = roi_compatible
+
+    return out
+
+def selective_crop(image):
+    r = cv2.selectROI("Crop select", image)
+    crop_image = image[int(r[1]):int(r[1] + r[3]),
+    int(r[0]):int(r[0] + r[2])]
+    cv2.destroyWindow("Crop select")
+    return crop_image
 
 def resize(image, width, height):
     image_resize = cv2.resize(image,(width, height))
@@ -29,6 +102,8 @@ def flip(image, orientation):
 
 def main():
     img = cv2.imread('100.jpg', 1)
+    img_const = cv2.imread('100.jpg', 1)
+    img_const2 = cv2.imread('100.jpg', 1)
 
     # Cropping parameters
     # Add some visual adjustment method - width/height = adjustable variable in GUI
@@ -52,8 +127,20 @@ def main():
     # Might just be 0 for vertical and 1 for horizontal in the flip function instead of 'vertical' and 'horizontal'
     orientation_flip = ['vertical', 'horizontal']
 
+    # Polygon selection demo
+    # Collecting points in np array
+    # pts = polygon_points(img_const2)
+    img_const2 = polygon_selection(img_const2)
+    cv2.imshow("Polygon selection", img_const2)
+    cv2.waitKey(0)
+
+    # Rectangular selection demo
+    img_const = rectangular_selection(img_const)
+    cv2.imshow("Selected image", img_const)
+    cv2.waitKey(0)
+
     # The cropped image will be the new main image
-    img = crop(img, crop_width_0, crop_width_1, crop_height_0, crop_height_1)
+    img = selective_crop(img)
     cv2.imshow("Cropped Image", img)
     cv2.waitKey(0)
 
@@ -67,14 +154,17 @@ def main():
     cv2.imshow("rotated", img)
     cv2.waitKey(0)
 
+    # The rotated image will now be the new main image
     img = rotation(img, direction_rotate[1])
     cv2.imshow("rotated 2", img)
     cv2.waitKey(0)
 
+    # The flipped image will now be the new main image
     img = flip(img, orientation_flip[1])
     cv2.imshow("Flipped", img)
     cv2.waitKey(0)
 
+    # The flipped image will now be the new main image
     img = flip(img, orientation_flip[0])
     cv2.imshow("Flipped 2", img)
     cv2.waitKey(0)
--- Paint++/main.py
+++ Paint++/main.py
@@ -1,59 +1,56 @@
-# imports different classes from the PyQt library
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QAction, QIcon, QKeySequence
-from PyQt6.QtWidgets import (
-    QApplication,
-    QWidget,
-    QPushButton,
-    QMainWindow,
-    QMenu,
-    QLabel,
-    QCheckBox,
-    QStatusBar,
-    QToolBar, QStyle,
-)
-
+import os
 import sys
 import cv2
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction, QIcon, QKeySequence, QPixmap, QImageReader, QImageWriter
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QStatusBar, QStyle,
+    QFileDialog, QMessageBox, QScrollArea, QMenu
+)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("Paint++")
         self.setMinimumSize(400, 300)
+        self.resize(900, 600)
 
-    #### This method creates the dropdown menu for File #####
-    #### It does not contain the operations of the buttons #####
+        self.image_label = QLabel("No image loaded", alignment=Qt.AlignmentFlag.AlignCenter)
+        self.scroll = QScrollArea()
+        self.scroll.setWidget(self.image_label)
+        self.scroll.setWidgetResizable(True)
+        self.setCentralWidget(self.scroll)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+        self.current_path = None
+
+    # --- Menyer ---
     def file_menu(self):
-
         menu = self.menuBar()
 
-        ##### New button to create a new image ####
         new_ = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_FileIcon), "New", self)
         new_.triggered.connect(self.toolbar_button_clicked)
 
-        ##### Open file in File-menu #####
         open_ = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_DirOpenIcon), "Open", self)
-        open_.triggered.connect(self.toolbar_button_clicked)
+        open_.triggered.connect(self.open_file)
 
-        ##### Save Button in File-menu #####
         save = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_DialogSaveButton), "Save", self)
-        save.triggered.connect(self.toolbar_button_clicked)
+        save.triggered.connect(self.save)
+        save.setShortcut(QKeySequence.StandardKey.Save)
 
-        ##### Save as Button in File-menu #####
         save_as = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_DialogSaveButton), "Save as", self)
-        save_as.triggered.connect(self.toolbar_button_clicked)
+        save_as.triggered.connect(self.save_as)
+        save_as.setShortcut(QKeySequence.StandardKey.SaveAs)
 
-        ##### Properties Button in File-menu
         properties = QAction(QIcon("icons/icons8-settings.svg"), "Settings", self)
         properties.triggered.connect(self.toolbar_button_clicked)
 
-        ##### Quit Button in File-menu #####
         quit_ = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_BrowserStop), "Exit Program", self)
-        quit_.triggered.connect(self.toolbar_button_clicked)
+        quit_.triggered.connect(self.close)
 
-        ##### Adds the Buttons to the menu #####
         file_menu = menu.addMenu("&File")
         file_menu.addAction(new_)
         file_menu.addAction(open_)
@@ -62,73 +59,144 @@ class MainWindow(QMainWindow):
         file_menu.addAction(properties)
         file_menu.addAction(quit_)
 
-
     def edit_menu(self):
         menu = self.menuBar()
-
         edit_menu = menu.addMenu("&Edit")
+
+        copy_action = QAction("Copy", self)
+        paste_action = QAction("Paste", self)
+        cut_action = QAction("Cut", self)
+
+        edit_menu.addAction(copy_action)
+        edit_menu.addAction(paste_action)
+        edit_menu.addAction(cut_action)
 
     def image_menu(self):
         menu = self.menuBar()
-
-        # Top-level Image menu
         image_menu = menu.addMenu("&Image")
 
         select_menu = QMenu("&Select", self)
-        image_menu.addMenu(select_menu)  # add as a submenu under Image
+        image_menu.addMenu(select_menu)
 
         rectangular = QAction(QIcon("icons/icons8-rectangular.svg"), "Rectangular", self)
-        #rectangular.triggered.connect(self.toolbar_button_clicked)
-
         lasso = QAction(QIcon("icons/icons8-lasso.svg"), "Lasso", self)
-
         polygon = QAction(QIcon("icons/icons8-polygon.svg"), "Polygon", self)
 
         select_menu.addAction(rectangular)
         select_menu.addAction(lasso)
         select_menu.addAction(polygon)
 
-        # Regular actions
         crop = QAction(QIcon("icons/icons8-crop.svg"), "Crop", self)
-
         resize = QAction(QIcon("icons/icons8-resize.svg"), "Resize", self)
-
         image_menu.addAction(crop)
         image_menu.addAction(resize)
 
-        # ---- Orientation submenu ----
         orientation_menu = QMenu("&Orientation", self)
-        image_menu.addMenu(orientation_menu)  # add as a submenu under Image
+        image_menu.addMenu(orientation_menu)
 
         rotate_right = QAction(QIcon("icons/icons8-rotate_right.svg"), "Rotate right", self)
-
         rotate_left = QAction(QIcon("icons/icons8-rotate_left.svg"), "Rotate left", self)
-
         flip_horizontal = QAction(QIcon("icons/icons8-flip_horizontal.svg"), "Flip horizontal", self)
-
         flip_vertical = QAction(QIcon("icons/icons8-flip_vertical.svg"), "Flip vertical", self)
 
-        # Add to submenu (grouped with a separator)
         orientation_menu.addAction(rotate_right)
         orientation_menu.addAction(rotate_left)
         orientation_menu.addAction(flip_horizontal)
         orientation_menu.addAction(flip_vertical)
 
+    # --- Funksjoner ---
     def toolbar_button_clicked(self):
-        print("click", s)
+        print("click")
+
+    def current_qimage(self):
+        pm = self.image_label.pixmap()
+        if pm is None:
+            return None
+        return pm.toImage()
+
+    def write_image(self, path: str, fmt: bytes | None):
+        img = self.current_qimage()
+        if img is None:
+            QMessageBox.information(self, "Nothing to save", "Load or create an image first.")
+            return False
+
+        writer = QImageWriter(path, fmt if fmt else b"")
+        ok = writer.write(img)
+        if not ok:
+            QMessageBox.critical(self, "Save Failed", writer.errorString() or "Unknown error.")
+            return False
+
+        self.current_path = path
+        self.status.showMessage(f"Saved: {os.path.basename(path)}")
+        return True
+
+    def open_file(self):
+        fmts = sorted(set(bytes(f).decode("ascii").lower() for f in QImageReader.supportedImageFormats()))
+        file_filter = "Images (" + " ".join(f"*.{ext}" for ext in fmts) + ");;All Files (*)"
+
+        path, _ = QFileDialog.getOpenFileName(self, "Open Image", "", file_filter)
+        if not path:
+            return
+
+        reader = QImageReader(path)
+        reader.setAutoTransform(True)
+        img = reader.read()
+        if img.isNull():
+            QMessageBox.critical(self, "Open Image Failed", f"{reader.errorString()}\n\nFile: {path}")
+            return
+
+        pix = QPixmap.fromImage(img)
+        self.image_label.setPixmap(pix)
+        self.image_label.adjustSize()
+
+        name = os.path.basename(path)
+        self.status.showMessage(f"Opened: {name} - {pix.width()}x{pix.height()} px")
+        self.setWindowTitle(f"Paint++ - {name}")
+
+    def save(self):
+        if not self.current_path:
+            self.save_as()
+            return
+
+        ext = os.path.splitext(self.current_path)[1].lower().lstrip(".")
+        fmt = ext.encode("ascii") if ext else None
+        self.write_image(self.current_path, fmt)
+
+    def save_as(self):
+        fmts = sorted(set(bytes(f).decode("ascii").lower() for f in QImageWriter.supportedImageFormats()))
+        pattern = " ".join(f"*.{ext}" for ext in fmts)
+        file_filter = f"Images ({pattern});;All Files (*)"
+
+        suggested = self.current_path or ""
+        path, _ = QFileDialog.getSaveFileName(self, "Save Image As", suggested, file_filter)
+        if not path:
+            return
+
+        root, ext = os.path.splitext(path)
+        if not ext:
+            ext = ".png"
+            path = root + ext
+
+        fmt = ext.lstrip(".").lower().encode("ascii")
+
+        if os.path.exists(path):
+            resp = QMessageBox.question(self, "Overwrite?", f"{os.path.basename(path)} exists. Overwrite?")
+            if resp != QMessageBox.StandardButton.Yes:
+                return
+
+        if self.write_image(path, fmt):
+            self.setWindowTitle(f"Paint++ - {os.path.basename(path)}")
+
 
 def main():
-
-    # creates an object from the QApplication class (classname() means to create object)
     app = QApplication(sys.argv)
-
-    # creates an object from the QWidget class
     window = MainWindow()
-    window.show() # IMPORTANT!!!!! Windows are hidden by default, so call the show method
     window.file_menu()
     window.edit_menu()
     window.image_menu()
+    window.show()
     app.exec()
+
 
 if __name__ == '__main__':
     main()

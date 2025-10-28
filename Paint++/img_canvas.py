7a29cdf4e32988d57f0940eee99838081925b350
SindreWinaes <124827071+SindreWinaes@users.noreply.github.com>
2025-10-28 13:50:42 +0100

    Canvas continue (#17)
    * temp before mergimg main into canvas
    
    * chore: ignore IDE and Python cache files
    
    * chore: ignore IDE files repo-wide
    
    * Update img_canvas.py and add Pipfile
    
    * started work on a class diagram, add usecase diagram to this file as well
    
    * updated class diagram
    
    * bug fix for canvas
    
    * resize canvas
    
    ---------
    
    Co-authored-by: Sindre Winæs <sindrew@uia.no>

--- Paint++/img_canvas.py
+++ Paint++/img_canvas.py
@@ -12,19 +12,32 @@ from PyQt6.QtWidgets import (
     QLabel,
     QCheckBox,
     QStatusBar,
-    QToolBar, QStyle, QFileDialog, QMessageBox, QScrollArea
+    QToolBar, QStyle, QFileDialog, QMessageBox, QScrollArea, QInputDialog
 )
 from PyQt6.uic.Compiler.qtproxies import QtWidgets
 
+
+##### Inhertis from Qwidget ######
 
 class Img_Canvas(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.image = None
-        self._checker = self.make_checker_brush(tile=16)
+        self.image = None                                       # No image loaded yet - Hodls QPixmap (image data)
+        self._checker = self.make_checker_brush(tile=16)        # Background Pattern
+        self.offset = QPoint(0, 0)                              # Sets the position of the image (for panning)
+        self.panning = False                                    # Is the image being dragged, Boolean value
+        self.Last_pos = None                                    # Last Mouse position
+
+
+27
+ 
+        self.image = None             
 
         self.offset = QPoint(0, 0)
+27
+ 
+        self.image = None             
         self.panning = False
         self.Last_pos = None
 
@@ -32,63 +45,93 @@ class Img_Canvas(QWidget):
         light = QColor("#eeeeee")
         dark = QColor("#bbbbbb")
 
-        pm = QPixmap(tile * 2, tile * 2)
+        pm = QPixmap(tile * 2, tile * 2)                        # Creates a 32x32 pixel image
         pm.fill(light)
 
         p = QPainter(pm)
-        p.fillRect(0, tile, tile, tile, dark)
-        p.fillRect(tile, 0, tile, tile, dark)
+        p.fillRect(0, tile, tile, tile, dark)                   # Bottom left square
+        p.fillRect(tile, 0, tile, tile, dark)                   # Top right Square
         p.end()
 
         return QBrush(pm)
 
+
+    ##### Loading Images #####
     def set_image(self, pix):
-        self.image = pix
-        self.offset = QPoint(0, 0)
-        self.resize(pix.size())
-        self.update()
-        print("[canvas] set_image:" , pix.size(), "null", pix.isNull())
+
+        self.image = pix                                        # Store the new image
+        self.offset = QPoint(0, 0)                              # Reset to center
+        self.resize(self.image.size())                          # Resize Widget to prefferd size
+        self.update()                                           # Redraw to show new image
 
 
     def paintEvent(self, event):
         p = QPainter(self)
-        p.fillRect(self.rect(), self._checker)
+        p.fillRect(self.rect(), self._checker)                  # Draw Checkerd Background
 
-        if self.image is None:
+        if self.image is None:                                  # Exits the method if there is no image
             return
 
+        # Calculates where to draw the image so it is centered
         x = (self.width() - self.image.width()) / 2
         y = (self.height() - self.image.height()) / 2
 
+        # Where the image drawn after the users dragged it (self.offset)
         xi = int(x + self.offset.x())
         yi = int(y + self.offset.y())
-        p.drawPixmap(xi, yi, self.image)
+        p.drawPixmap(xi, yi, self.image)                        # Draws the image with DrawPixmap
 
+        # Draw border around the image
         p.setPen(QColor("#888888"))
         p.drawRect(xi, yi, self.image.width()-1, self.image.height()-1)
 
 
     def sizeHint(self):
+        if self.image is not None:
+            return self.image.size()
         return QSize(800, 600)
 
 
+    ##### Mouse Press (Start Dragging) #####
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            self.panning = True
-            self.Last_pos = event.position().toPoint()
-            self.setCursor(Qt.CursorShape.ClosedHandCursor)
+            self.panning = True                                 # Start Panning mode
+            self.Last_pos = event.position().toPoint()          # Remember where we clicked
+            self.setCursor(Qt.CursorShape.ClosedHandCursor)     # Change cursor to hand
 
+
+    ##### Mouse movement while dragging #####
     def mouseMoveEvent(self, event):
         if self.panning and self.Last_pos is not None:
-            pos = event.position().toPoint()
-            delta = pos - self.Last_pos
-            self.offset += delta
-            self.Last_pos = pos
-            self.update()
 
+            pos = event.position().toPoint()                    # Current mouse position
+            delta = pos - self.Last_pos                         # The Change in mouse position
+            self.offset += delta                                # Move the image by that amount
+            self.Last_pos = pos                                 # Update for next frame
+            self.update()                                       # Trigger paintEvent to redraw, handled by Qt's Event loop
+
+
+    ##### Mouse Release (stop dragging) #####
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            self.panning = False
+            self.panning = False                                # Stop Panning mode
             self.Last_pos = None
-            self.setCursor(Qt.CursorShape.ArrowCursor)
+            self.setCursor(Qt.CursorShape.ArrowCursor)          # Change back to normal cursor
+
+
+    ##### Allows User to Change he canvas Size
+    def resize_canvas(self):
+
+        width, ok1 = QInputDialog.getInt(self, "Canvas Width", "Enter Width", 800, 100, 5000 )
+        if not ok1:     # User Clicked cancel
+            return
+
+        height, ok2 = QInputDialog.getInt(self, "Canvas Height", "Enter Height", 800, 100, 5000 )
+        if not ok2:     # User Clicked Cancel
+            return
+
+        self.resize(width, height)
+        self.update()
+
+
 
--- Paint++/main.py
+++ Paint++/main.py
@@ -30,7 +30,7 @@ def main():
 
     # creates an object from the QWidget class
     window = MainWindow()
-    window.show() # IMPORTANT!!!!! Windows are hidden by default, so calle the show method
+    window.show()
     window.file_menu()
     window.edit_menu()
     window.image_menu()
@@ -44,42 +44,22 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Paint++")
         self.resize(1920, 1080)
 
-
-        self.scroll = QScrollArea()
-
-        container = QWidget()
-        layout = QVBoxLayout(container)
-
-        self.canvas = Img_Canvas(container)
-
-        layout.addWidget(self.canvas, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
+        self.canvas = Img_Canvas()                                  # Creates an instance of the canvas class
+        self.scroll = QScrollArea()                                 # Creates a scroll area
 
 
-        self.scroll.setWidget(container)
-        self.scroll.setWidgetResizable(True)
-        self.setCentralWidget(self.scroll)
-
-
-        #self.image_label = QLabel("No image loaded", alignment=Qt.AlignmentFlag.AlignCenter)
-        #self.scroll = QScrollArea()
-        #self.scroll.setWidget(self.image_label)
-        #self.scroll.setWidgetResizable(True)
-        #self.setCentralWidget(self.scroll)
-
+        self.scroll.setWidget(self.canvas)                          # Puts the canvas inside the scroll area
+        self.scroll.setWidgetResizable(False)
+        self.scroll.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(self.scroll)                          # Makes tbe scroll are the main content
         self.status = QStatusBar()
         self.setStatusBar(self.status)
 
-        self.current_path = None
+        self.current_path = None                                    # Tracks current file path
 
-
-
-    # this is a test-branch for git/pr
 
 
     #### This method creates the dropdown menu for File #####
-    #### It does not contain the operations of the buttons #####
     def file_menu(self):
 
         menu = self.menuBar()
@@ -163,9 +143,14 @@ class MainWindow(QMainWindow):
         cut_action.setShortcut(QKeySequence.StandardKey.Cut) #cut function
         cut_action.triggered.connect(self.toolbar_button_clicked)
 
+        # Allows user to change the canvas size, uses the resize_canvas method from img_canvas
+        canvas_size = QAction(self.style().standardIcon(QStyle.StandardPixmap.SP_FileDialogDetailedView), "Canvas Size", self)
+        canvas_size.triggered.connect(self.canvas.resize_canvas)
+
         edit_menu.addAction(paste_action)
         edit_menu.addAction(cut_action)
         edit_menu.addAction(copy_action)
+        edit_menu.addAction(canvas_size)
 
     def image_menu(self):
         menu = self.menuBar()
@@ -204,20 +189,23 @@ class MainWindow(QMainWindow):
         print("click", s)
 
 
-    def current_qimage(self):
-        pm = self.image_label.pixmap()
-        if pm is None:
+    def current_image(self):
+        if self.canvas.image is None:
             return None
-        return pm.toImage()
+        return self.canvas.image.toImage()
 
+    ##### Actually saves the file #####
     def write_image(self, path: str, fmt: bytes | None):
-        img = self.current_qimage()
+
+
+        img = self.current_qimage()             # Get Current image
         if img is None:
             QMessageBox.information(self, "Nothing to save", "Load or create an image first.")
             return False
 
         writer = QImageWriter(path, fmt  if fmt else b"")
-        ok = writer.write(img)
+
+        ok = writer.write(img)                   # Write to disk
         if not ok:
             QMessageBox.critical(self, "Save Failed", writer.errorString() or "Unkown error.")
             return False
@@ -226,48 +214,66 @@ class MainWindow(QMainWindow):
         self.status.showMessage(f"Saved: {os.pathj.basename(path)}")
         return True
 
+
+
     def open_file(self):
+
+        # Get supported formats
        fmts = sorted(set(bytes(f).decode("ascii").lower() for f in QImageReader.supportedImageFormats()))
        file_filter = "Images (" + " ".join(f"*.{ext}" for ext in fmts) + ");;All Files (*)"
 
+
+        # Show file dialog
        path, _ = QFileDialog.getOpenFileName(self, "Open Image", "", file_filter)
+
+        # User cancelled
        if not path:
            return
 
-       reader = QImageReader(path)
-       reader.setAutoTransform(True)
+       reader = QImageReader(path)      # Try to read the image
+       reader.setAutoTransform(True)    # Auto-rotate based on EXIF data
        img = reader.read()
-       if img.isNull():
+       if img.isNull():                 # Failed to load
            QMessageBox.critical(self, "Open Image Failed", f"{reader.errorString()}\n\nFile: {path}")
            return
 
+
+        # Convert to Qpixmap for Display
+        # Use img for pixel processing like filters
        pix = QPixmap.fromImage(img)
        if pix.isNull():
            QMessageBox.critical(self, "Open Image Failed", f"Loaded QImage but QPixmap is null. \nFile: {path}")
            return
 
-
+        # Update UI
        name = os.path.basename(path)
        self.status.showMessage(f"Opened: {name} -  {pix.width()}x{pix.height()} px")
        self.setWindowTitle(f"Paint++ - {name}")
 
-       self.canvas.set_image(pix)
-       vp = self.scroll.viewport().size()
-       desired = QSize(max(pix.width(), vp.width()), max(pix.height(), vp.height()))
-       self.canvas.resize(desired)
+
+        # Send Image to Canvas
+       self.canvas.set_image(pix)                       # Calls the set_image method from img_canvas
+       self.scroll.horizontalScrollBar().setValue(0)
+       self.scroll.verticalScrollBar().setValue(0)
+
        self.current_path = path
 
 
     def save(self):
-        if not self.current_path:
-            self.save_as()
+        if not self.current_path:                       # Never saved before?
+            self.save_as()                              # Ask Where to Save
             return
 
+        # Get File extensions
         ext = os.path.splitext(self.current_path)[1].lower().lstrip(".")
         fmt = ext.encode("ascii") if ext else None
-        self.write_image(self.current_path, fmt)
+        self.write_image(self.current_path, fmt)        # Do the Actual Save
+
+
 
     def save_as(self):
+
+        # Get supported write formats
         fmts = sorted(set(bytes(f).decode("ascii").lower() for f in QImageWriter.supportedImageFormats()))
         pattern = " ".join(f"*.{ext}" for ext in fmts)
         file_filter = f"Images ({pattern});;All Files (*)"
@@ -287,6 +293,7 @@ class MainWindow(QMainWindow):
 
         fmt = ext.lstrip(".").lower().encode("ascii")
 
+        # Check if overwrtiting
         if os.path.exists(path):
             resp = QMessageBox.question(self, "Overwrite?", f"{os.path.basename(path)} exists. Overwrite?")
             if resp != QMessageBox.StandardButton.Yes:
